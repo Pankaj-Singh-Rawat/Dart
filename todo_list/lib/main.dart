b06bfb78e1f5e7aa5c772ff2ca898fe4a81064b5
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-04-08 02:33:12 +0530

    Created grid style notes - Incomplete State

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -15,85 +15,144 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(title: 'Todo Lists'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter(){
-    setState(() {
-      _counter--;
-    });
-  }
+  List<String> notes = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: (){
+            // open menu drawer
+          },
+          tooltip: "Menu",
+        ),
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+
+      //Main App Body that will show Notes will come up Here
+
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          itemCount: notes.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //defines how many cards(to write notes) will come in a row
+            crossAxisCount: 2, // for this app -> 2
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            ), 
+
+          itemBuilder: (context, index){
+            // builds each individual note card
+            return NoteCard(noteText: "Note ${index + 1}",);
+          }
+          ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
 
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: FloatingActionButton(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-            ),
-          ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:() async {
+          final newNote = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const NoteEditorPage(),
+              ),
+          );
 
-          const SizedBox(width: 100,),
+          //if note is not empty , add it 
 
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-          ),
+          if (newNote?.isNotEmpty == true){
+            setState(() {
+              notes.add(newNote!);
+            });
+          }
+        },
+        tooltip: "New Note",
+        child: const Icon(Icons.add),
+      ),
+    );
 
+  }
+}
 
-          const SizedBox(width: 30,),
+class NoteCard extends StatelessWidget {
+  final String noteText;
+  const NoteCard({super.key, required this.noteText});
 
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // each note appears with a clean card with text in it
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.deepPurple[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Text(noteText),
     );
   }
 } 
+
+class NoteEditorPage extends StatefulWidget{
+  const NoteEditorPage({super.key});
+
+  @override
+  State<NoteEditorPage> createState() => _NoteEditorPageState();
+}
+
+class _NoteEditorPageState extends State<NoteEditorPage>{
+  String noteText = "";
+
+  @override
+Widget build(BuildContext context) {
+  return WillPopScope(
+    onWillPop: () async {
+      Navigator.pop(context, noteText.trim());
+      return true;
+    },
+    child: Scaffold(
+      appBar: AppBar(
+        title: const Text("New Note"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          autofocus: true,
+          maxLines: null,
+          onChanged: (value) {
+            setState(() {
+              noteText = value;
+            });
+          },
+          decoration: const InputDecoration(
+            hintText: "New note here...",
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+}
